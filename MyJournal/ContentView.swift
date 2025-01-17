26f6ce73db128755b487e90a209882333fe978f5
PhucTran <tranhieuphuc12@gmail.com>
2025-01-17 08:57:22 +0700

    Initial Commit

---
+++ MyJournal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyJournal
+//
+//  Created by DaiTran on 17/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyJournal/MyJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJournalApp.swift
+//  MyJournal
+//
+//  Created by DaiTran on 17/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MyJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
